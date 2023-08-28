00d5dca20a80d27e14ca544d5f8e85ff38bde82f
sergehas <55877559+sergehas@users.noreply.github.com>
2023-08-28 18:03:57 +0200

    fix share module declaration

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TableExpandableRowsComponent } from './components/table-expandable-rows/table-expandable-rows.component';
-import { InstanceofPipe } from './pipes/instanceof.pipe';
-import { ColumnEditorComponent } from './components/column-editor/column-editor.component';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { TabsNavComponent } from './components/tabs-nav/tabs-nav.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { DragDropModule } from "@angular/cdk/drag-drop";
+// import { InstanceofPipe } from "./pipes/instanceof.pipe";
+// import { TableExpandableRowsComponent } from "./components/table-expandable-rows/table-expandable-rows.component";
+// import { TableConfigEditorComponent } from "./components/table-config-editor/table-config-editor.component";
+// import { TabsNavComponent } from "./components/tabs-nav/tabs-nav.component";
 
 @NgModule({
-  declarations: [
-    TableExpandableRowsComponent,
-    InstanceofPipe,
-    ColumnEditorComponent,
-    TabsNavComponent
-  ],
-  imports: [
-    CommonModule,
-    DragDropModule
-  ]
+	declarations: [
+		// TableExpandableRowsComponent,
+		// InstanceofPipe,
+		// TableConfigEditorComponent,
+		// TabsNavComponent,
+	],
+	imports: [CommonModule, DragDropModule],
 })
-export class SharedModule { }
+export class SharedModule {}
