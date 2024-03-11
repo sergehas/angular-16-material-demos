cb48d7d0652f940ad366e15230bb65aa44808402
S.HASSOUN <serge.hassoun@capgemini.com>
2024-03-11 09:48:07 +0100

    add conf. for export demo

--- src/app/demo/pages/demo-export/demo-export.component.ts
+++ src/app/demo/pages/demo-export/demo-export.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { Item } from 'src/app/core/item/models/item';
 import { ItemService } from 'src/app/core/item/services/item.service';
 import { PageableDataSource, Paginator } from 'src/app/core/models/pageable-data-source';
@@ -8,7 +9,7 @@ import { ExcelExportService } from 'src/app/core/services/excel-export.service';
 @Component({
   selector: 'app-demo-export',
   templateUrl: './demo-export.component.html',
-  styleUrl: './demo-export.component.scss'
+  styleUrls: ['./demo-export.component.scss']
 })
 export class DemoExportComponent implements AfterViewInit {
   exportEvents: string[] = [];
@@ -16,14 +17,18 @@ export class DemoExportComponent implements AfterViewInit {
   constructor(readonly exportService: ExcelExportService, readonly dataService: ItemService) {
     //console.debug(`initializing datasource`);
   }
+  cols = new FormControl(5, [Validators.required]);
+  rows = new FormControl(37, [Validators.required]);
 
   export(): void {
     let dataSource = new PageableDataSource<Item, Paginator>(this.dataService);
     const p = new Paginator(100);
     p.pageSize = 100;
+    this.dataService.itemCount=this.rows.value!;
+    this.dataService.attributeCount=this.cols.value!;
     dataSource.paginator = p;
     dataSource.length$.subscribe(l => {
-      console.log(`set export datsource length to ${l}`);
+      console.log(`set export datasource length to ${l}`);
     })
 
     //for demo, reset pager
