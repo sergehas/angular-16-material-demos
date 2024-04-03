e2fe4664704e445ae4dcae906897b810de165a78
S.HASSOUN <serge.hassoun@capgemini.com>
2024-04-03 09:51:52 +0200

    add demo page for notif

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -1,38 +1,39 @@
-import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { MatProgressBarModule } from "@angular/material/progress-bar";
-import { MatTableModule } from "@angular/material/table";
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatSortModule } from "@angular/material/sort";
-import { MatIconModule } from "@angular/material/icon";
-import { MatButtonModule } from "@angular/material/button";
-import { MatBadgeModule } from "@angular/material/badge";
-import { MatDividerModule } from "@angular/material/divider";
+import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MatSlideToggleModule } from "@angular/material/slide-toggle";
-import { MatInputModule } from "@angular/material/input";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatSliderModule } from "@angular/material/slider";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatButtonModule } from "@angular/material/button";
 import { MatDatepickerModule } from "@angular/material/datepicker";
-import { MomentDateAdapter } from "@angular/material-moment-adapter";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from "@angular/material/select";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatSliderModule } from "@angular/material/slider";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
 
 import { TranslateModule } from "@ngx-translate/core";
 
-import { DemoRoutingModule } from "./demo-routing.module";
-import { TableExpandableRowsComponent } from "../shared/components/table-expandable-rows/table-expandable-rows.component";
-import { TabsNavComponent } from "../shared/components/tabs-nav/tabs-nav.component";
 import { IconSelectComponent } from "../shared/components/icon-select/icon-select.component";
 import { IconTreeComponent } from "../shared/components/icon-tree/icon-tree.component";
+import { TableExpandableRowsComponent } from "../shared/components/table-expandable-rows/table-expandable-rows.component";
+import { TabsNavComponent } from "../shared/components/tabs-nav/tabs-nav.component";
+import { DemoRoutingModule } from "./demo-routing.module";
 
-import { DemoComponent } from "./pages/demo.component";
-import { DemoTableComponent } from "./pages/demo-table/demo-table.component";
-import { DemoDatasourceComponent } from "./pages/demo-datasource/demo-datasource.component";
-import { VoidNavComponent } from "./pages/void-nav/void-nav.component";
-import { DemoIconsComponent } from "./pages/demo-icons/demo-icons.component";
-import { DemoI18nComponent } from "./pages/demo-i18n/demo-i18n.component";
-import { DemoExportComponent } from "./pages/demo-export/demo-export.component";
 import { LocalizedDatePipe } from "../shared/pipes/translation/localized-date.pipe";
-import { MatSelectModule } from "@angular/material/select";
+import { DemoDatasourceComponent } from "./pages/demo-datasource/demo-datasource.component";
+import { DemoExportComponent } from "./pages/demo-export/demo-export.component";
+import { DemoI18nComponent } from "./pages/demo-i18n/demo-i18n.component";
+import { DemoIconsComponent } from "./pages/demo-icons/demo-icons.component";
+import { DemoNotifComponent } from "./pages/demo-notif/demo-notif.component";
+import { DemoTableComponent } from "./pages/demo-table/demo-table.component";
+import { DemoComponent } from "./pages/demo.component";
+import { VoidNavComponent } from "./pages/void-nav/void-nav.component";
 
 @NgModule({
 	declarations: [
@@ -43,6 +44,7 @@ import { MatSelectModule } from "@angular/material/select";
 		VoidNavComponent,
 		DemoIconsComponent,
 		DemoI18nComponent,
+		DemoNotifComponent,
 	],
 	imports: [
 		CommonModule,
@@ -55,6 +57,7 @@ import { MatSelectModule } from "@angular/material/select";
 		MatBadgeModule,
 		MatDividerModule,
 		FormsModule,
+		MatRadioModule,
 		MatSlideToggleModule,
 		MatInputModule,
 		MatFormFieldModule,
@@ -74,4 +77,4 @@ import { MatSelectModule } from "@angular/material/select";
 		DemoRoutingModule,
 	],
 })
-export class DemoModule {}
+export class DemoModule { }
---
+++ src/app/demo/pages/demo-notif/demo-notif.component.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { Notification, NotificationService, NotificationSeverity } from 'src/app/core/services/notification.service';
+
+
+type NotifLog = {
+  id: string,
+  message: string,
+  action: "add" | "update";
+}
+
+@Component({
+  selector: 'app-demo-notif',
+  templateUrl: './demo-notif.component.html',
+  styleUrl: './demo-notif.component.scss'
+})
+export class DemoNotifComponent {
+
+  readonly notifs: NotifLog[] = [];
+  show = false;
+  severity: NotificationSeverity = "info";
+  persistent = true;
+
+
+
+  constructor(
+    private service: NotificationService,
+  ) {
+    this.service.notification$.subscribe((n) => this.logNotif(n));
+  }
+  clear() {
+    this.service.clear();
+  }
+
+
+  addNotif(): void {
+    let cnt = Math.floor(Math.random() * 101);
+    let notif = this.service.notify(
+      new Notification({
+        severity: this.severity,
+        message:
+          cnt % 3 === 0
+            ? `message #${cnt}: notif message body, notif message body, notif message body, notif message body, notif message body, notif message body `
+            : `short message # ${cnt}`,
+        show: this.show,
+        persistent: this.persistent
+      })
+    );
+  }
+
+  addRandom(): void {
+    let cnt = Math.floor(Math.random() * 101);
+    const severity = ["info", "warn", "sever"][Math.floor(Math.random() * 3)] as NotificationSeverity;
+    const show = cnt % 4 !== 0;
+    this.service.notify(
+      new Notification({
+        severity: severity,
+        message:
+          cnt % 3 === 0
+            ? `message #${cnt}: random message body, random message body, random message body, random message body, random message body, random message body `
+            : `short random message # ${cnt}`,
+        show: !(cnt % 4 === 0),
+      }));
+
+  }
+
+
+  private logNotif(notif: Notification, action: "add" | "update" = "add"): void {
+    this.notifs.push({ message: notif.message, id: notif.id, action: action });
+  }
+}
+
