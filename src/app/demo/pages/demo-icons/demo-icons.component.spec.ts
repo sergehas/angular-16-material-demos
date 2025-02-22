ccd30526d2f147797ca8d6a577cc81c764268bd6
S.HASSOUN <serge.hassoun@capgemini.com>
2025-02-22 16:22:01 +0100

    fix ut

--- src/app/demo/pages/demo-icons/demo-icons.component.spec.ts
+++ src/app/demo/pages/demo-icons/demo-icons.component.spec.ts
@@ -1,6 +1,26 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, inject, TestBed } from "@angular/core/testing";
 
+import { provideHttpClient } from "@angular/common/http";
+import { provideHttpClientTesting } from "@angular/common/http/testing";
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { Category } from "src/app/core/icons/models/category";
+import { IconsService } from "src/app/core/icons/services/icons.service";
+import { DemoModule } from "../../demo.module";
 import { DemoIconsComponent } from "./demo-icons.component";
+
+/**
+ * Mock implementation of IconService for testing purposes.
+ */
+class MockIconsService {
+
+  getIconslib(): Category {
+    const r = new Category("root");
+    r.addCategory("brands").addIcon("azure");
+    return r;
+  }
+}
 
 describe("DemoIconsComponent", () => {
   let component: DemoIconsComponent;
@@ -8,14 +28,28 @@ describe("DemoIconsComponent", () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [DemoModule, NoopAnimationsModule],
       declarations: [DemoIconsComponent],
-    });
+      providers: [provideHttpClient(),
+      provideHttpClientTesting(),
+      { provide: IconsService, useClass: MockIconsService },
+        // { provide: MatIconRegistry, useClass: FakeMatIconRegistry }
+      ],
+    }).compileComponents();
     fixture = TestBed.createComponent(DemoIconsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it("should create", () => {
+
+  beforeEach(inject([MatIconRegistry, DomSanitizer], (mir: MatIconRegistry, sanitizer: DomSanitizer) => {
+    // The `MatIconRegistry` will make GET requests to fetch any SVG icons that are in the registry. More on this below...
+    const sanitizedUrl = sanitizer.bypassSecurityTrustResourceUrl("/fakeIconPath");
+    // Make sure that the icon name matches the icon name your component would be looking up.
+    mir.addSvgIconInNamespace("parameter", "brands-azure", sanitizedUrl);
+  }));
+
+  it("should create", async () => {
     expect(component).toBeTruthy();
   });
 });
--- src/app/demo/pages/demo-icons/demo-icons.component.ts
+++ src/app/demo/pages/demo-icons/demo-icons.component.ts
@@ -1,16 +1,19 @@
-import { Component, ViewEncapsulation } from "@angular/core";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { FormControl, Validators } from "@angular/forms";
 
-//import { IconsService } from "src/app/core/icons/services/icons.service";
 @Component({
   selector: "app-demo-icons",
   templateUrl: "./demo-icons.component.html",
   styleUrls: ["./demo-icons.component.scss"],
   encapsulation: ViewEncapsulation.None,
 })
-export class DemoIconsComponent {
-  //constructor(private service: IconsService) {}
+export class DemoIconsComponent implements OnInit {
   selected: string | null = "parameter:brands-azure";
 
   icon = new FormControl("", [Validators.required]);
+
+  ngOnInit(): void {
+    this.icon.setValue(this.selected);
+  }
+
 }
