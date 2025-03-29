774257593d53d226548fd36e0dbc22a9fd09173b
S.HASSOUN <serge.hassoun@capgemini.com>
2025-03-29 19:11:25 +0100

    refactor: convert @Input() to new input() function for roles in AnyRolesDirective

--- src/app/shared/directives/all-roles.directive.ts
+++ src/app/shared/directives/all-roles.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, EventEmitter, Input, OnInit, Output, inject } from "@angular/core";
+import { Directive, ElementRef, EventEmitter, OnInit, Output, inject, input } from "@angular/core";
 import { LoginService, Role } from "src/app/core/services/login.service";
 
 @Directive({
@@ -9,15 +9,17 @@ export class AnyRolesDirective implements OnInit {
   private readonly _elementRef = inject(ElementRef);
   private readonly _loginService = inject(LoginService);
 
-  @Input("appAllRoles") roles: string[] | Role[] | undefined = [];
+  readonly roles = input<string[] | Role[] | undefined>([], { alias: "appAllRoles" });
+
   @Output() granted = new EventEmitter<boolean>();
+
   ngOnInit() {
     if (!this.roles || this.roles.length === 0) {
       console.log(`[appAllRoles] no roles provided [${this.roles}]: grant access`);
       this.granted.emit(true);
       return;
     }
-    const hasAccess = this._loginService.getLoggedUser().hasAllRoles(this.roles as Role[]);
+    const hasAccess = this._loginService.getLoggedUser().hasAllRoles(this.roles() as Role[]);
     console.log(`[appAllRoles] has access for roles [${this.roles}]: ${hasAccess}`);
     this.granted.emit(hasAccess);
     //still usefull ?
