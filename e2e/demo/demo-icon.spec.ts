88a0fe1548959b035fedfd8ae4d532144ebe812f
S.HASSOUN <serge.hassoun@capgemini.com>
2025-08-19 17:11:23 +0200

    test: update demo icon selector tests for improved visibility checks

--- e2e/demo/demo-icon.spec.ts
+++ e2e/demo/demo-icon.spec.ts
@@ -3,7 +3,16 @@ import { expect, test } from "@playwright/test";
 test("it should open mat-form-field icon selector", async ({ page }) => {
   await page.goto("/");
   await page.getByRole("link", { name: "demo" }).click();
-  await expect(page.locator("#mat-menu-panel-2")).not.toBeVisible();
-  await page.locator("#app-icon-select-2").getByRole("button", { name: "Select a icon" }).click();
-  await expect(page.locator("#mat-menu-panel-2")).toBeVisible();
+  await page.getByRole("tab", { name: "demo-icons" }).click();
+  await page.getByRole("button", { name: "CLEAR" }).click();
+  // no selected icon
+  await expect(page.getByRole("code").filter({ hasText: /^$/ })).toBeVisible();
+  await page
+    .locator("mat-form-field")
+    .filter({ hasText: "icon ?" })
+    .getByLabel("Select a icon")
+    .click();
+  await expect(
+    page.locator("#mat-menu-panel-2 div").filter({ hasText: "brands Azure expand_more" })
+  ).toBeVisible();
 });
