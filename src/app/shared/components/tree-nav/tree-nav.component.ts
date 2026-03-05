37c9285a853e4bc8969cd6bb0b613b16c3e9bf46
S.HASSOUN <serge.hassoun@capgemini.com>
2026-03-05 11:07:03 +0100

    feat(tree-nav): add rail menu toggle and update styles for improved navigation

--- src/app/shared/components/tree-nav/tree-nav.component.ts
+++ src/app/shared/components/tree-nav/tree-nav.component.ts
@@ -1,11 +1,11 @@
 import { FlatTreeControl } from "@angular/cdk/tree";
-import { Component, ViewEncapsulation, inject } from "@angular/core";
-import { MatTreeFlatDataSource, MatTreeFlattener, MatTreeModule } from "@angular/material/tree";
-import { Router, RouterModule, RouterOutlet } from "@angular/router";
-
+import { Component, ViewEncapsulation, inject, signal } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
 import { MatIconModule } from "@angular/material/icon";
 import { MatListModule } from "@angular/material/list";
+import { MatTreeFlatDataSource, MatTreeFlattener, MatTreeModule } from "@angular/material/tree";
+import { Router, RouterModule, RouterOutlet } from "@angular/router";
 import { MenuNode, NavBuilder } from "../tabs-nav/models/nav-builder";
 
 /** Flat node with expandable and level information */
@@ -20,25 +20,30 @@ interface MenuFlatNode {
 @Component({
   selector: "app-tree-nav",
   standalone: true,
-  imports: [MatIconModule, MatTreeModule, MatListModule, MatButtonModule, RouterModule],
+  imports: [
+    MatIconModule,
+    MatTreeModule,
+    MatListModule,
+    MatButtonModule,
+    RouterModule,
+    MatButtonToggleModule,
+  ],
   templateUrl: "./tree-nav.component.html",
   styleUrl: "./tree-nav.component.scss",
   encapsulation: ViewEncapsulation.None,
 })
 export class TreeNavComponent {
   private readonly router = inject(Router);
+  isRail = signal(false);
 
   constructor() {
     //menu content
     this.dataSource.data = NavBuilder.buildTree("", this.router.config);
     console.info("[app-tree-nav] menu datasource", this.dataSource.data);
   }
-
   prepareRoute(outlet: RouterOutlet) {
-    console.info(
-      `[app-tree-nav] prepareRoute ${outlet?.activatedRouteData && outlet.activatedRouteData["animation"]}`
-    );
-    return outlet?.activatedRouteData && outlet.activatedRouteData["animation"];
+    console.info(`[app-tree-nav] prepareRoute ${outlet?.activatedRouteData?.["animation"]}`);
+    return outlet?.activatedRouteData?.["animation"];
   }
   //menu
   private readonly _transformer = (node: MenuNode, level: number): MenuFlatNode => {
